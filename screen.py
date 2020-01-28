3a85859c71821fa8922e28a2e4ad77febe0a04de
anbreaker <antunez19@gmail.com>
2020-01-28 19:32:16 +0100

    Posicionamiento de textos, titulo centrado

--- screen.py
+++ screen.py
@@ -4,9 +4,14 @@ import pygame
 LARGO = 700
 ANCHO = 500
 
+# Definicion tamaños de textos
+ALTO_TEXTO_TITULOS = 30
+ALTO_TEXTO_DESCRIPCIONES = 22
+
 # Definimos algunos colores
-NEGRO = (0, 0, 0)
+VERDE = (30, 186, 22)
 BLANCO = (255, 255, 255)
+NEGRO = (0, 0, 0)
 
 # Fotogramas por segundo
 FPS = 60
@@ -22,10 +27,11 @@ def main():
     fondo_pantalla = pygame.image.load('resources/background.png').convert()
 
     # Titulo de la barra de la aplicacion
-    pygame.display.set_caption("The Quest Juego pyGame")
+    pygame.display.set_caption('The Quest Juego pyGame')
 
     # Fuente para el texto que aparecerá en pantalla (tamaño 30)
-    fuente = pygame.font.Font('resources/fonts/alatsi.ttf', 30)
+    fuente_titulo = pygame.font.Font('resources/fonts/alatsi.ttf', ALTO_TEXTO_TITULOS)
+    fuente_descripciones = pygame.font.Font('resources/fonts/alatsi.ttf', ALTO_TEXTO_DESCRIPCIONES)
 
     # Iteramos hasta que el usuario haga click sobre le botón de salida.
     # Condicion de salida del bucle principal
@@ -54,33 +60,48 @@ def main():
 
         if pagina_de_instrucciones == 1:
             # Instrucciones de dibujo, página 1
-            # También podría cargar una imagen realizada por cualquier otro programa.
-            # De esta forma sería más fácil y flexible.
+            # Podría cargar una imagen realizada por cualquier otro programa.
+            # de esta forma sería más fácil y flexible.
 
             # Texto por lineas y posicion en pantalla
-            linea_texto1 = fuente.render(
-                "Bienvenido al Juego The Quest:", True, BLANCO)
+            linea_texto1 = fuente_titulo.render(
+                'Bienvenidos a El juego: The quest!', True, VERDE)
             # Para centrar el texto mido su tamaño con esta funcion que devuelve w,h
             ancho_linea_texto1 = linea_texto1.get_rect().width
             # Calculo del posicionamiento de linea_texto1
             posicion_centrada = (LARGO/2)-(ancho_linea_texto1/2)
-            # Presentacion del texto
+            # Presentacion del texto en pantalla
             pantalla.blit(linea_texto1, [posicion_centrada, 10])
-            
+
 
             # Texto por lineas y posicion en pantalla
-            linea_texto2 = fuente.render(
-                "Historia del juego: La búsqueda comienza en un planeta tierra moribundo por el cambio climático. Partiremos a la búsqueda de un planeta compatible con la vida humana para colonizarlo.", True, BLANCO)
-            pantalla.blit(linea_texto2, [10, 44])
+            linea_texto2 = fuente_descripciones.render('Historia del juego: La búsqueda comienza en un planeta tierra', True, BLANCO)
+            # pantalla.blit(linea_texto2, [10, ALTO_TEXTO_TITULOS + 15])
+            
+            # Texto por lineas y posicion en pantalla
+            linea_texto3 = fuente_descripciones.render('moribundo por el cambio climático. Partiremos a la búsqueda de', True, BLANCO)
+            # pantalla.blit(linea_texto3, [10, (ALTO_TEXTO_TITULOS * 2 + 10) + 5])
+            
+            # Texto por lineas y posicion en pantalla
+            linea_texto4 = fuente_descripciones.render('un planeta compatible con la vida humana para colonizarlo.', True, BLANCO)
+            # pantalla.blit(linea_texto4, [10, ALTO_TEXTO_DESCRIPCIONES + 5])
+
+            # Texto por lineas y posicion en pantalla
+            linea_texto5 = fuente_descripciones.render('Esquiva los obstaculos, hazte con el control de la nave y logra aterrizar!', True, BLANCO)
+            # pantalla.blit(linea_texto5, [10, ALTO_TEXTO_DESCRIPCIONES + 5])
+            
+            
+             
+            
 
         if pagina_de_instrucciones == 2:
             # Instrucciones de dibujo, página 2
             linea_texto1 = fuente.render(
-                "Pantalla de instrucciones", True, BLANCO)
+                'Pantalla de instrucciones', True, BLANCO)
             pantalla.blit(linea_texto1, [10, 10])
 
             # Texto por lineas y posicion en pantalla
-            linea_texto2 = fuente.render("Página 2", True, BLANCO)
+            linea_texto2 = fuente.render('Página 2', True, BLANCO)
             pantalla.blit(linea_texto2, [10, 44])
 
         # # Limitamos a 20 fotogramas por segundo.
@@ -100,7 +121,7 @@ def main():
     pygame.quit()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # Inicialización de Pygame
     pygame.init()
     main()
