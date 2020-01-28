62e522d98760493c2471463a39f05ec7f18b687f
anbreaker <antunez19@gmail.com>
2020-01-28 16:48:41 +0100

    Tratando de arrancarlo

--- screen.py
+++ screen.py
@@ -1,10 +1,15 @@
 import pygame
 
+# Variables de uso global
+LARGO = 700
+ANCHO = 500
+
 # Definimos algunos colores
 NEGRO = (0, 0, 0)
 BLANCO = (255, 255, 255)
-FPS = 60  # Fotogramas por segundo
 
+# Fotogramas por segundo
+FPS = 60  
 
 def main():
     # Inicialización de Pygame
@@ -12,12 +17,15 @@ def main():
 
     # Inicialización de la superficie de dibujo (display surface)
     # Establecemos el largo y ancho de la pantalla.
-    dimensiones = [700, 500]
+    dimensiones = [LARGO, ANCHO]
     pantalla = pygame.display.set_mode(dimensiones)
     pygame.display.set_caption("Pantalla de Instrucciones")
 
     # Inicializacion de la imagen de fondo de la pantalla (sin efecto alpha)
-    fondo_pantalla = pg.image.load('resources/background.png').convert()
+    fondo_pantalla = pygame.image.load('resources/background.png').convert()
+
+    # Fuente para el texto que aparecerá en pantalla (tamaño 30)
+    fuente = pygame.font.Font('resources/fonts/alatsi.ttf', 30)
 
     # Iteramos hasta que el usuario haga click sobre le botón de salida.
     # Condicion de salida del bucle principal
@@ -34,13 +42,10 @@ def main():
     rect_cambio_x = 5
     rect_cambio_y = 5
 
-    # Fuente para el texto que aparecerá en pantalla (tamaño 30)
-    fuente = pygame.font.Font('resources/fonts/alatsi.ttf', 30)
-
     mostrar_instrucciones = True  # Bandera para condicion
     pagina_de_instrucciones = 1   # Bandera
 
-    # Bucle de la Página de Instrucciones -----------
+    # Bucle de las Páginas de Instrucciones -----------
     while not salir and mostrar_instrucciones:
         for evento in pygame.event.get():   # El usuario hace algo
             if evento.type == pygame.QUIT:  # Si el usuario hace click en cerrar
@@ -54,6 +59,40 @@ def main():
 
     # Limpia la pantalla y establece su color de fondo
     pantalla.blit(fondo_pantalla, (0, 0))
+
+    if pagina_de_instrucciones == 1:
+        # Instrucciones de dibujo, página 1
+        # También podría cargar una imagen realizada por cualquier otro programa.
+        # De esta forma sería más fácil y flexible.
+
+        # Texto por lineas y posicion en pantalla
+        linea_texto1 = fuente.render("Instrucciones...\n ver", True, BLANCO)
+        pantalla.blit(linea_texto1, [10, 10])
+        
+        # Texto por lineas y posicion en pantalla
+        linea_texto2 = fuente.render("Página 1", True, BLANCO)
+        pantalla.blit(linea_texto1, [10, 44])
+        
+    if pagina_de_instrucciones == 2:
+        # Instrucciones de dibujo, página 2
+        linea_texto1 = fuente.render("Este programa hace rebotar un rectángulo", True, BLANCO)
+        pantalla.blit(linea_texto1, [10, 10])
+        
+        # Texto por lineas y posicion en pantalla
+        linea_texto2 = fuente.render("Página 2", True, BLANCO)
+        pantalla.blit(linea_texto2, [10, 44])
+        
+    # Bucle Principal del Programa -----------
+    while not salir:
+        for evento in pygame.event.get(): # El usuario hace algo
+            if evento.type == pygame.QUIT: # Si el usuario hace click en cerrar
+                salir = True
+        # Limpia la pantalla y establece su color de fondo
+        pantalla.blit(fondo_pantalla, (0, 0))
+        
+    
+        
+    # Para salir correctamente de la aplicacion y cierre todos los procesos
     pygame.quit()
 
 
