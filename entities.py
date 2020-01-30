38d4ccf17ff25246cd076c0f31c3fe57e29e8a77
anbreaker <antunez19@gmail.com>
2020-01-30 17:48:29 +0100

    Posicionada nave, no funciona alpha

--- entities.py
+++ entities.py
@@ -8,8 +8,23 @@ FPS = 60
 
 
 class Rocket(pygame.sprite.Sprite):
-    pict_rocket = 'resources/rocket.png'
+    pict_rocket = 'ball.png'
     w_pict_rocket = 68
     h_pict_rocket = 40
 
     def __init__(self, x=0, y=(ANCHO/2)-h_pict_rocket):
+        self.x = x
+        self.y = y
+        
+        # Obtiene el rectangulo (preguntar bien que hace)
+        pygame.sprite.Sprite.__init__(self)
+        
+        # Inicializacion de la imagen del player (es un rectangulo)
+        self.image = pygame.image.load(f'resources/{self.pict_rocket}').convert_alpha()
+        # Convertimos la imagen en un rectangulo con w,h, -> devuelve (0,0,68,40)
+        self.rect = self.image.get_rect()
+        # Coordenadas de entrada para posicionamiento -> Pos(x,y)
+        self.rect.x = x
+        self.rect.y = y
+        
+
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import pygame
+import pygame 
 from pygame.locals import *
 import sys
+
+from entities import *
 
 # Variables de uso global
 
@@ -28,6 +30,14 @@ class Juego:
         
         # Inicializacion de la imagen de fondo de la pantalla (sin efecto alpha)
         self.fondo_pantalla = pygame.image.load('resources/background.png').convert()
+        
+        # Creamos la instancia del jugador
+        self.player = Rocket()
+        
+        # Creacion de grupo de Sprite
+        self.allSprintes = pygame.sprite.Group()
+        # Agregamos al grupo al jugador
+        self.allSprintes.add(self.player)
 
     def game_over(self):
         pygame.quit()
@@ -37,16 +47,25 @@ class Juego:
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
                 self.game_over()
+            if evento.type == MOUSEBUTTONDOWN:
+                self.game_over()
+                
     
     def main_loop(self):
         while True:
-            tiempo_transcurrido = self.clock.tick(FPS)
+            # tiempo_transcurrido
+            dt = self.clock.tick(FPS)
             
             # Llamamos al broker de eventos
             self.manejar_eventos()
             
             # Limpia la pantalla y establece el fondo
             self.pantalla.blit(self.fondo_pantalla, (0,0))
+            
+            # Actualizamos y pintamos los grupos de Sprite para mostrarlos en pantalla
+            self.allSprintes.update(dt)
+            self.allSprintes.draw(self.pantalla)
+            
             
             # Actualizamos la pantalla con lo dibujado.
             pygame.display.flip()
@@ -58,4 +77,4 @@ if __name__ ==  '__main__':
     juego.main_loop()
     
     # https://plataforma.keepcoding.io/courses/714386/lectures/13745010
-    # MINUTO 2.29   
+    # MINUTO 2.45
