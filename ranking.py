ec9fadd39ae3aed4de9a35f8bf68f8e432b562ac
anbreaker <antunez19@gmail.com>
2020-02-10 22:18:33 +0100

    formateado codigo ranking

--- ranking.py
+++ ranking.py
@@ -1,4 +1,3 @@
-
 import sqlite3
 
 
@@ -7,23 +6,20 @@ class Ranking():
     # Para implementar las llamadas a la BBDD
     # ranking = Ranking()
     # ranking.mostrar_ranking()
-    
-    def create_table(self,c):
+
+    def create_table(self, c):
         c.execute("CREATE TABLE IF NOT EXISTS `ranking` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `user` TEXT NOT NULL, `point` INTEGER NOT NULL)")
-    
-    def data_entry(self,c,conn):
+
+    def data_entry(self, c, conn):
         c.execute("INSERT INTO ranking (user, point) VALUES('SJA',650)")
         conn.commit()
         c.close()
         conn.close()
 
     def mostrar_ranking(self):
-        
+
         conn = sqlite3.connect('ranking.db')
         c = conn.cursor()
-        
+
         self.create_table(c)
-        self.data_entry(c,conn)
-        
-        
-        
+        self.data_entry(c, conn)
