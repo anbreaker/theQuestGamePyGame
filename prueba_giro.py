8b8581ffab1a4498891855d896af4b50f0a299a2
anbreaker <antunez19@gmail.com>
2020-02-21 16:20:51 +0100

    pruebas giro...

--- prueba_giro.py
+++ prueba_giro.py
@@ -1,9 +1,7 @@
-# Setup Python ----------------------------------------------- #
 from pygame.locals import *
 import pygame
 import sys
 
-# Setup pygame/window ---------------------------------------- #
 mainClock = pygame.time.Clock()
 pygame.init()
 pygame.display.set_caption('giro de nave')
@@ -13,11 +11,9 @@ image_nave = pygame.image.load('resources/images/rocket.png')
 
 image_nave_180 = 0
 
-# Loop ------------------------------------------------------- #
 while True:
 
-    # Background --------------------------------------------- #
-    screen.fill((0, 20, 0))
+    screen.fill((0, 0, 0))
 
     if image_nave_180 < 180:
         image_nave_180 += 1
@@ -27,7 +23,6 @@ while True:
     image_nave_copia = pygame.transform.rotate(image_nave_copia, image_nave_180)
     screen.blit(image_nave_copia, (200, 200))
 
-    # Buttons ------------------------------------------------ #
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -37,6 +32,5 @@ while True:
                 pygame.quit()
                 sys.exit(0)
 
-    # Update ------------------------------------------------- #
     pygame.display.update()
     mainClock.tick(60)
